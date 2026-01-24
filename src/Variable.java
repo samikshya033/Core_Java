3b41a5dde6e74874bbaaf269fb62682096ce0c13
Samubhusal <samakshyabhusal066@gmail.com>
2026-01-24 19:03:40 +0545

    division variable

--- src/Variable.java
+++ src/Variable.java
@@ -1,5 +1,34 @@
 //Division one:
-//
-public class Variable {
+//Based on type of value represented by the available all variable are
+// divide into two type==>
 
-}
+//1.Primitive variables can be used to represent primitive value
+//eg:int x =10;
+
+//2. Reference variables can be used to refer objects
+//eg:Student S=new Student();
+//s is pointing to object
+
+//Division two:
+//Based on the position of declaration under behavior all variables are
+// divided into three types==>
+//1. Instance variables
+//2.Static     "
+//3.Local Variables
+
+//1.Instance Variables:
+//a)if the value of variable is varied from object to object
+//such type of variables are called instance variables.
+//b)for every object separate copy of instance variables will be created
+
+// Instance variable within the class directly but outside
+// any method block or constructor
+
+//
+
+
+//public class Variable {
+//    string name;
+//    int roll no;
+//
+//}
